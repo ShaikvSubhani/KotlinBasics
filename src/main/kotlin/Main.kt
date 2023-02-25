2d7f5f58a16e36b0e0c70fae6a379cb63cbefc42
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-02-25 11:46:20 +0530

    basics

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+
+
+//here name is top level variable
+var name:String="sunil"
+var greeting:String?="hello"
+
+fun main() {
+    println("Hello World!");
+    println(greeting)
+    //variables in kotlin
+    //difference between var and val
+    //after assigning to val you cant change the data
+    //but in var you can change
+    greeting="hi";
+
+    println(greeting)
+
+    //in kotlin you can also declare variables outside the main function,
+    //those variables are called top level variables
+    var name: String="subhani"
+    name="shubham";
+
+    // types are not null in kotlin
+    println(name);
+
+
+    //conditional statements in kotlin are same as java
+    if(greeting !=null)
+        println(greeting)
+    else
+        println(greeting);
+
+    //like a switch case in java , kotlin has whne
+    when(greeting)
+    {
+        null-> println("hi")
+        //in when else case is like default case when none of the above case is true, then else case get executed
+        else-> println("not null")
+    }
+}
