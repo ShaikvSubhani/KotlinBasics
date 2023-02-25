7dfbbefcfa452a000bc4a0303bf7eabc710b4d36
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-02-25 12:19:08 +0530

    functions with parameters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,20 @@
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS IN KOTLIN
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+fun Greeting(): String{
+
+    return "hello getGreeting"
+}
+
+//function with arguments
+
+fun param(someVar: String)
+{
+    val msg= "hello "+ someVar;
+    println(msg)
+}
+
 
 //here name is top level variable
 var name:String="sunil"
@@ -37,4 +53,10 @@ fun main() {
         //in when else case is like default case when none of the above case is true, then else case get executed
         else-> println("not null")
     }
+
+    println(Greeting())
+
+    //calling the paramterized function
+    param("Subhani")
+
 }
